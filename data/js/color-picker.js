189f2dd11d7407d4fa05f366a634cee306b6aa5b
shyuuun <m4rshm4ll0w14@gmail.com>
2025-10-06 23:56:54 +0800

    feat: added pages, and also changed the folder struct

---
+++ data/js/color-picker.js
@@ -0,0 +1,16 @@
+function colorPick() {
+	return {
+		isOn: false,
+		color: "#ff0000",
+		brightness: 128,
+
+		applyColor() {
+     console.log("Applying settings:");
+      console.log("LED Power:", this.isOn ? "On" : "Off");
+      console.log("Color:", this.color);
+      console.log("Brightness:", this.brightness);
+      // Here you would typically send the settings to your backend or device
+      }
+    },
+	};
+}
